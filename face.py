7d42f2f28eb7546d33b95b5d4453fec8c01ea349
yth98 <b06901031@ntu.edu.tw>
2018-12-20 19:14:33 +0800

    face landmark implemented

---
+++ face.py
@@ -0,0 +1,63 @@
+# Ref: https://github.com/davisking/dlib/blob/master/python_examples/face_landmark_detection.py
+
+# set_image(self: dlib.image_window, image: object)
+# add_overlay(self: dlib.image_window, rectangles: dlib.rectangles, color: dlib.rgb_pixel=rgb_pixel(255,0,0))
+# add_overlay(self: dlib.image_window, detection: dlib.full_object_detection, color: dlib.rgb_pixel=rgb_pixel(0,0,255))
+
+import cv2
+import dlib
+import time
+import numpy
+
+time.clock()
+ts=time.clock()
+
+if(__name__=='__main__'): pass
+else:
+    global face, face_proc
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    print("FACE Init time:",time.clock()-ts,"s")
+
+    def face(img):
+        if type(img) is str:
+            img = cv2.imread(img)
+            if img is None: return None
+        d_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        dets = detector(d_img, 1) # dlib.dlib.rectangles
+        faces = []
+        for n in range(len(dets)):
+            for k, d in enumerate([dets[n]]):   # dlib.dlib.rectangle
+                shape = predictor(img, d)       # dlib.dlib.full_object_detection
+                if(shape.num_parts != 68): break
+            pt = []
+            for i in range(68):
+                pt.append((shape.part(i).x,shape.part(i).y))
+            faces += [[(dets[n].left(), dets[n].top()), (dets[n].right(), dets[n].bottom()), pt]]
+        return faces
+
+    def face_draw(src):
+        rects = face(src)
+        pix = src.copy()
+        #pix = numpy.zeros((*(pix.shape[0:2]),3), numpy.uint8)
+        for rect in rects:
+            if(not rect is None):
+                cv2.rectangle(pix, *(rect[0:2]), (0,0,255))
+                for i in range(0,16):
+                    cv2.line(pix, rect[2][i], rect[2][i+1], (255,0,0))
+                for i in range(17,21):
+                    cv2.line(pix, rect[2][i], rect[2][i+1], (255,255,0))
+                for i in range(22,26):
+                    cv2.line(pix, rect[2][i], rect[2][i+1], (255,255,0))
+                for i in range(27,30):
+                    cv2.line(pix, rect[2][i], rect[2][i+1], (255,0,0))
+                for i in range(31,35):
+                    cv2.line(pix, rect[2][i], rect[2][i+1], (255,0,0))
+                for i in range(36,41):
+                    cv2.line(pix, rect[2][i], rect[2][i+1], (128,255,0))
+                for i in range(42,47):
+                    cv2.line(pix, rect[2][i], rect[2][i+1], (128,255,0))
+                for i in range(48,67):
+                    cv2.line(pix, rect[2][i], rect[2][i+1], (0,255,0))
+        return pix
---
+++ face_landmark.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from math import floor, ceil
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import face
+
+
+# In[2]:
+
+
+# return [B,G,R,A]
+def get_pixel(y, x, src):
+    if (x<0 or y<0 or x>src.shape[1]-1 or y>src.shape[0]-1): # out of frame
+        return [0,0,0,0]
+    xt, yt = x-int(x), y-int(y)
+    #print(xt,yt)
+    u, d, l, r = floor(y), ceil(y), floor(x), ceil(x)
+    ul, ur, dl, dr = src[u][l], src[u][r], src[d][l], src[d][r]
+    bgr = np.zeros(3)
+    bgr += ul * xt     * yt
+    bgr += ur * (1-xt) * yt
+    bgr += dl * xt     * (1-yt)
+    bgr += dr * (1-xt) * (1-yt)
+    return np.concatenate([bgr,[1]])
+
+
+# In[3]:
+
+
+# magniture of vector
+def mag(x):
+    return np.sqrt(x.dot(x))
+# angle is clockwise or counterclockwise
+def cross_direction(x, y):
+    return np.sign(x[0]*y[1]-x[1]*y[0])
+# project y on x , ratio of x
+def projection_factor(x, y):
+    return x.dot(y) / x.dot(x)
+
+
+# In[4]:
+
+
+a, b, p = 0.01, 2, 1
+def morph(dest, src):
+    pairs = [[(0,0),(0,300),(0,0),(180,240)],[(100,0),(400,0),(90,10),(350,90)]] # srcP,srcQ,destP,destQ
+    for i in range(len(pairs)):
+        for j in range(4): pairs[i][j] = np.array(pairs[i][j])
+    for y in range(dest.shape[0]):
+        for x in range(dest.shape[1]):
+            X = np.array([x,y])
+            X_s = np.zeros(2)
+            weights = 0
+            for pair in pairs:
+                PQ = pair[3]-pair[2]
+                P_Q_ = pair[1]-pair[0]
+                u = projection_factor(PQ, X-pair[2]) # scalar, u*PQ is the actual u
+                v = X-pair[2]-u*PQ
+                v_ = P_Q_[::-1]*np.array([-1,1])/mag(PQ) * mag(v)*cross_direction(PQ,X-pair[2])
+                X_ = (pair[0]*(1-u) + pair[1]*u) + v_
+                #print(X, X_, u,v, v_, cross_direction(PQ,X-pair[2]))
+                weight = (mag(PQ)**p/(a+mag(v)))**b
+                X_s += (X_-X) * weight
+                weights += weight
+            X_s = X + X_s/weights
+            dest[y][x] = get_pixel(X_s[1], X_s[0], src)[0:3]
+
+
+# In[5]:
+
+
+source = cv2.imread('./img/person_1/Ming Ouhyoung.png')
+destination = np.zeros(source.shape).astype(np.uint8)
+print(source.shape) # (height, width, BGR)
+#morph(destination, source)
+destination = face.face_draw(source)
+
+
+# In[6]:
+
+
+cv2.imshow('image', source)
+cv2.waitKey(0)
+cv2.imshow('image', destination)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('./detect.jpg', destination)
+
+
+# In[7]:
+
+
+plt.imshow(cv2.cvtColor(source, cv2.COLOR_BGR2RGB))
+plt.show()
+plt.imshow(cv2.cvtColor(destination, cv2.COLOR_BGR2RGB))
+plt.show()
+
+
+# In[ ]:
+
+
+
+
