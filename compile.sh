03f6ec971315159dabfdc3eb084095847021b955
yth98 <b06901031@ntu.edu.tw>
2018-12-27 12:37:23 +0800

    migrate dlib face landmark

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #g++ -o single single.cpp `pkg-config opencv --cflags --libs`
 #g++ -o multi multi.cpp `pkg-config opencv --cflags --libs`
 g++ -o landmark landmark.cpp `pkg-config dlib-1 --cflags --libs` -lpthread -lX11 -ljpeg
-./landmark ./img/person_1/image004.jpg
+./landmark ./img/person_1/image004.jpg python/shape_predictor_68_face_landmarks.dat
--- landmark.cpp
+++ landmark.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "dlib/gui_widgets.h"
 #include "dlib/image_io.h"
+#include "dlib/image_processing.h"
 #include "dlib/image_processing/frontal_face_detector.h"
+#include "dlib/image_processing/render_face_detections.h"
 
 using namespace dlib;
 using std::cout;
@@ -9,23 +11,33 @@ using std::cin;
 using std::endl;
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        cout << "Usage: landmark <image_path>\n";
+    if (argc < 3) {
+        cout << "Usage: landmark <image_path> <shape_predictor_68_face_landmarks.dat_path>\n";
         return -1;
     }
 
+    frontal_face_detector detector = get_frontal_face_detector();
+    shape_predictor predictor;
+    deserialize(argv[2]) >> predictor;
 
     array2d<unsigned char> img;
     load_image(img, argv[1]);
 
-    frontal_face_detector detector = get_frontal_face_detector();
     std::vector<rectangle> dets = detector(img);
     cout << "Number of faces detected: " << dets.size() << endl;
+
+    std::vector<full_object_detection> faces;
+    for (unsigned int i = 0; i < dets.size(); ++i) {
+        full_object_detection shape = predictor(img, dets[i]);
+        cout << "number of parts: "<< shape.num_parts() << endl;
+        faces.push_back(shape);
+    }
 
     image_window win;
     win.clear_overlay();
     win.set_image(img);
     win.add_overlay(dets, rgb_pixel(255,0,0));
+    win.add_overlay(render_face_detections(faces));
 
     cout << "Hit enter..." << endl;
     cin.get();
